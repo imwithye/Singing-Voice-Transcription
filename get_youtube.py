15f9d1bc7121f52454c58baa9bc2cbfb28f80bc7
Yiwei Gong <imwithye@gmail.com>
2023-11-21 06:31:16 +0000

    get youtube

---
+++ get_youtube.py
@@ -0,0 +1,88 @@
+import os
+import shutil
+from utils import read_json
+from joblib import Parallel, delayed
+from tqdm import tqdm
+from pathlib import Path
+import yt_dlp
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATASET_DIR = os.path.join(PROJECT_DIR, "dataset")
+
+LINKS_JSON_FILE = os.path.join(PROJECT_DIR, "links.json")
+LINKS = read_json(LINKS_JSON_FILE)
+
+
+def fetch_audio(filepath: str, link: str):
+    class Logger(object):
+        def debug(self, msg):
+            pass
+
+        def warning(self, msg):
+            pass
+
+        def error(self, msg):
+            pass
+
+    fp = Path(filepath)
+    ydl_opts = {
+        "outtmpl": str(fp.with_suffix("")) + ".%(ext)s",
+        "format": "bestaudio/best",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "320",
+            }
+        ],
+        "logger": Logger(),
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
+
+
+def get_youtube(idx, dir):
+    output = os.path.join(DATASET_DIR, dir, str(idx), "Mixture.mp3")
+    if os.path.exists(output):
+        return True, None
+    try:
+        os.makedirs(os.path.dirname(output), exist_ok=True)
+        link = LINKS[str(idx)]
+        fetch_audio(output, link)
+        return True, None
+    except:
+        return False, f"Download failed for idx {idx}: {link}"
+
+def cleanup(dir):
+    for idx in range(1, len(LINKS)+1):
+        output = os.path.join(DATASET_DIR, dir, str(idx), "Mixture.mp3")
+        parent = os.path.dirname(output)
+        if not os.path.exists(output) and os.path.exists(parent):
+            shutil.rmtree(parent)
+
+
+if __name__ == "__main__":
+    print("Downloading training set")
+    # song #1~#400 are training set
+    results = Parallel(n_jobs=4)(
+        delayed(get_youtube)(idx, "train") for idx in tqdm(range(1, 401))
+    )
+    failed = [r for r in results if not r[0]]
+    print(f"Failed to download {len(failed)} songs")
+    for f in failed:
+        print(f[1])
+
+    print("Downloading training set")
+    # song #401~ are valid set
+    results = Parallel(n_jobs=4)(
+        delayed(get_youtube)(idx, "valid") for idx in tqdm(range(401, len(LINKS)+1))
+    )
+    failed = [r for r in results if not r[0]]
+    print(f"Failed to download {len(failed)} songs")
+    for f in failed:
+        print(f[1])
+    
+    print("Cleaning up")
+    # remove empty directories
+    cleanup("train")
+    cleanup("valid")
